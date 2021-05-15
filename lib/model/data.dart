4c01c4f65715144c4b288427e4a4b157d1d7c5fc
redsolver <info@redsolver.net>
2021-05-15 14:08:55 +0200

    add fromJson

--- lib/model/data.dart
+++ lib/model/data.dart
@@ -4,42 +4,53 @@ class QuizObject {
   String videoLink;
   List<QuizQuestion> questions = <QuizQuestion>[];
 
-  QuizObject({this.fach = "mathe", 
-    this.topic = "",  this.videoLink = ""});
+  QuizObject({this.fach = "mathe", this.topic = "", this.videoLink = ""});
+
+  factory QuizObject.fromJson(Map<String, dynamic> json) => QuizObject(
+        fach: json["fach"],
+        topic: json["topic"],
+        videoLink: json["videoLink"],
+      )..questions = List<QuizQuestion>.from(
+          json["questions"].map((x) => QuizQuestion.fromJson(x)));
 
   Map toJson() => {
-    "fach" : fach,
-    "topic" : topic,
-    "videoLink" : videoLink,
-    "questions" : questions,
-  };
+        "fach": fach,
+        "topic": topic,
+        "videoLink": videoLink,
+        "questions": questions,
+      };
 }
 
 class QuizQuestion {
   String content;
   QuizQuestionType type;
   List<QuizAnswer> answers = <QuizAnswer>[];
-  QuizQuestion({this.content = "",
-    this.type = QuizQuestionType.multipleChoice});
-  
-  Map toJson() => {
-    "content" : content,
-    "type" : type.toString(),
-    "answers" : answers
-  };
+  QuizQuestion(
+      {this.content = "", this.type = QuizQuestionType.multipleChoice});
+
+  factory QuizQuestion.fromJson(Map<String, dynamic> json) => QuizQuestion(
+        content: json["content"],
+        type: {
+          'QuizQuestionType.multipleChoice': QuizQuestionType.multipleChoice,
+        }[json["type"]],
+      )..answers = List<QuizAnswer>.from(
+          json["answers"].map((x) => QuizAnswer.fromJson(x)));
+
+  Map toJson() =>
+      {"content": content, "type": type.toString(), "answers": answers};
 }
 
 class QuizAnswer {
   String content;
   bool correct;
-  QuizAnswer({this.content = "",
-    this.correct = false});
-    Map toJson() => {
-    "content" : content,
-    "correct" : correct
-  };
+  QuizAnswer({this.content = "", this.correct = false});
+
+  factory QuizAnswer.fromJson(Map<String, dynamic> json) => QuizAnswer(
+        content: json["content"],
+        correct: json["correct"],
+      );
+
+  Map toJson() => {"content": content, "correct": correct};
 }
 
-enum QuizQuestionType{
-  multipleChoice
-}
+enum QuizQuestionType { multipleChoice }
